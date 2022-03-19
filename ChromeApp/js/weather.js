6c77c5e918846c234dd0bcb4237d0c4bab676f3b
simhani1 <simhani1@gmail.com>
2022-03-20 05:44:43 +0900

    weather complete

---
+++ ChromeApp/js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "b5915294428c3c1d920792e10d76b906";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("You live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+    });
+}
+
+function onGeoError() {
+  console.log("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
